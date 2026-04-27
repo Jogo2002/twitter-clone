c4f2a980071dff2e96ddda2f3d586a55b162c67b
jogo2002 <jdgoldhaber@icloud.com>
2026-04-27 11:57:24 -0700

    make templating generic

--- main.py
+++ main.py
@@ -4,13 +4,26 @@ Starts a hello world webserver.
 
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import uvicorn
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/home", response_class=HTMLResponse)
 async def index(request: Request):
-    return 'hello <b>world<b>'
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+    )
+
+@app.get("/login", response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="login.html",
+    )
+
 
 if __name__ == "__main__":
     uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
